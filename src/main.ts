d38000bf6e12a969f4b7b60be014adfc778e30e6
Farruh Zoirov <fzoirov29@gmail.com>
2024-06-24 03:41:05 +0500

    task completed

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import {ValidationPipe} from "@nestjs/common";
+import * as dotenv from 'dotenv';
+import * as process from "node:process";
+dotenv.config();
+
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,7 +20,7 @@ async function bootstrap() {
     transform: true,
   }));
 
-  await app.listen(4000);
+  await app.listen(process.env.APP_PORT || 5000);
 }
 
 bootstrap();
